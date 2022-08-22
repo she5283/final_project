2933a3ffa2a717e6630a340d67c23b369e7d5623
eosu <she5823@naver.com>
2022-08-22 14:43:25 +0900

    swagger, security 설정 추가

--- zproject02/src/main/java/com/example/demo/Zproject02Application.java
+++ zproject02/src/main/java/com/example/demo/Zproject02Application.java
@@ -12,4 +12,13 @@ public class Zproject02Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Zproject02Application.class, args);
 	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		// 입력길이에 상관없이 일정한 길이의 결과가 나오는 함수 : hash함수
+		// 예) 1234%10->4, 174%10->4
+		// 비밀번호는 해시함수로 암호화(해독 불가능)
+		// 암호화 작업에는 항상 랜덤이 필요 -> 비밀번호의 경우 salt 필요(자동처리)
+		return new BCryptPasswordEncoder();
+	}
 }
